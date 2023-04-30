0a414137d28ce2a5af0847850ac15fa6865a4944
pavel fomichev <pfomicev68@gmail.com>
2023-04-30 22:17:20 +0300

    исправил критические ошибки

--- scripts/index.js
+++ scripts/index.js
@@ -2,15 +2,17 @@ let profile = document.querySelector('.profile');
 let popup = document.querySelector('.popup');
 let form = popup.querySelector('.popup__container');
 let edit = profile.querySelector('.profile__edit-button');
-let close = popup.querySelector('.popup__container-close');
-let inputname = popup.querySelector('.popup__container-item');
-let inputinfo = popup.querySelector('.popup__container-item:last-child');
+let close = popup.querySelector('.popup__close-button');
+let inputname = popup.querySelector('.popup__input_type_nickname');
+let inputinfo = popup.querySelector('.popup__input_type_info');
 let title = profile.querySelector('.profile__title');
 let subtitle = profile.querySelector('.profile__subtitle');
 let submit = popup.querySelector('.popup__container-button');
 
 edit.addEventListener('click', function () {
     popup.classList.add('popup_opened');
+    inputname.value = title.textContent;
+    inputinfo.value = subtitle.textContent;
 });
 
 submit.addEventListener('click', function () {
@@ -18,8 +20,6 @@ submit.addEventListener('click', function () {
 
     function handleFormSubmit (evt) {
         evt.preventDefault();
-        inputname.getAttribute('value');
-        inputinfo.getAttribute('value');
         title.textContent = inputname.value;
         subtitle.textContent = inputinfo.value;
     };
